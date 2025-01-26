d3672bd669bdc7e739f671c845ebdc1eed438914
Pengskii09 <yojie9@gmail.com>
2025-01-27 01:12:46 +0800

    css

--- menu.js
+++ menu.js
@@ -1,9 +1,23 @@
-var audio = new Audio("Updated Picture/Assets/main_menu_music.mp3");
-    audio.volume = 0.1;
-    audio.loop = true;
-    audio.autoplay = true;
+// Background Music
+const backgroundAudio = new Audio("Updated Picture/Assets/main_menu_music.mp3");
+backgroundAudio.volume = 0; // Initial volume
+backgroundAudio.loop = true;
+backgroundAudio.autoplay = true;
 
-    let volume = document.getElementById('volume');
-    volume.addEventListener("change", function(e) {
-        audio.volume = e.currentTarget.value / 1000;
-    })
+// Volume control for background music
+let volumeControl = document.getElementById('volume');
+volumeControl.addEventListener("change", function (e) {
+    backgroundAudio.volume = e.currentTarget.value / 1000; // Adjust volume based on slider
+});
+
+// Button Hover Sound
+const buttons = document.querySelectorAll('.trash-dash-buttons-container button');
+
+// Add event listener to play sound when the button is hovered
+buttons.forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        const hoverAudio = new Audio('Sound Effects/buttonhover3.mp3'); // Create a new audio object for the hover sound
+        hoverAudio.volume = 0.3; 
+        hoverAudio.play();
+    });
+});
